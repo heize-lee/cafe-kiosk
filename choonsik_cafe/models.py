18eb8038c24092557e060bcb27a0332df83daaa0
Heize <dev.heize@gmail.com>
2024-04-08 15:36:28 +0900

    docs: add README

--- choonsik_cafe/models.py
+++ choonsik_cafe/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+# from django.db import models
+
+# # Create your models here.
+# # models안에 Model이라는 클래스를 상속받자.
+# class Category(models.Model):
+#     name = models.CharField(max_length=30)
+# #연결할거니까 포링키, 어떤거랑 연결할거냐?, ON-delete, what is cascade?
+# class Item:
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=30)
+#     price = models.IntegerField()
+# class Order:
+#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+#     order_dt = models.DateTimeField(auto_now_add=True)
+#     item_count = models.IntegerField()
+#     order_price = models.IntegerField()
+
 # menu: menu_id, {option_id}, drinks, sub_drinks, price 
 class Menu(models.Model):
     menu_id = models.AutoField(primary_key=True, verbose_name='Menu ID')
@@ -8,6 +25,7 @@ class Menu(models.Model):
     sub_drink = models.CharField(verbose_name='Sub Drink', max_length=100)
     price = models.IntegerField(verbose_name='Price')
 
+    #매직메서드 return self.name
     def __str__(self):
         return f"{self.drink} - {self.sub_drink}"
     
--- choonsik_cafe/views.py
+++ choonsik_cafe/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Menu, Order, Payment
 
-# Create your views here.
+def rich_cafe(request):
+    categorys = Menu.objects.all()
+    context = {
+        "lions" : categorys
+    }
+    return render(request, 'choonsik_cafe/rich_cafe.html', context)
