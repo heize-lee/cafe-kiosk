d07589504b06255f88e6cd46af2e161a47c36f16
Heize <dev.heize@gmail.com>
2024-04-18 15:02:27 +0900

    test4

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,6 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static/'
 
-STATICFILES_DIRS = [BASE_DIR/'static']
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
